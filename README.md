b696b1c71726917576164665d1c7de493d625021
Aleksandar Matevski <matevskial@gmail.com>
2024-08-09 23:23:55 +0200

    Implement simple http server that returns status code 404

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ChirpyX - my implementation of chirpy, a project built while following boot.dev course for backend development in go
+
+# How to compile and run
+
+Use the command specified below to build and run:
+
+`go build -o out && ./out`
--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	httpServeMux := http.NewServeMux()
+
+	/* note that if serveMux does not have any handler, the is a handler that returns 404 status code
+	I added a custom handler just for demonstration purposes
+	*/
+	httpServeMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		http.NotFound(w, req)
+	})
+
+	httpServer := http.Server{
+		Handler: httpServeMux,
+		Addr:    ":8080",
+	}
+
+	err := httpServer.ListenAndServe()
+	/* note that call to log.Fatal blocks in this case, meaning  the server is running
+	As of now, I am not sure why it blocks, maybe there is a channel that waits to receive an actual error value?
+	*/
+	log.Fatal(err)
+}
