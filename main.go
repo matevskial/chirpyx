ccaaa7b6d2dfc53f715fff228b4dc97a5ce3845b
Aleksandar Matevski <matevskial@gmail.com>
2024-08-09 23:38:47 +0200

    Implement simple file server for serving static content

--- main.go
+++ main.go
@@ -6,12 +6,20 @@ import (
 )
 
 func main() {
+	staticContentDir := http.Dir(".")
+	httpFileServer := http.FileServer(staticContentDir)
+
 	httpServeMux := http.NewServeMux()
 
 	/* note that if serveMux does not have any handler, the is a handler that returns 404 status code
-	I added a custom handler just for demonstration purposes
+	I added a custom handler for 404 status code just for demonstration purposes
+	This is the way I found to make server return index.html for / and 404 for any other paths.
 	*/
 	httpServeMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/" {
+			httpFileServer.ServeHTTP(w, req)
+			return
+		}
 		http.NotFound(w, req)
 	})
 
